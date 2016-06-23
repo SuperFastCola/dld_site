eef84025fa0c6f3f8d33cc58781e0f542e1d3805
Anthony Baker <anthony@Anthony-Bakers-MacBook-Pro.local>
2016-06-23 13:52:51 -0400

    Updating Angular Site

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,11 +1,44 @@
 #!/bin/sh
 
-if [ ! -z "$1" ]; then
-  if [ -e "$1" ] ; then
-	aws s3api put-object --bucket portfolio.deluxeluxury.com --key $1 --body $1  --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
+#!/bin/sh
+#change user,group and perms for multplie directoies
+
+directorypattern="(css)"
+filepattern="(html|json)"
+excludeslash="\/"
+bucket="superfastcola.com"
+bucketfullpath="s3://$bucket/"
+uploadfrom=${PWD} #can put an entire path here - right now set to current working directory	
+
+if [ -d "$uploadfrom" ]; then
+	for i in `ls -al $uploadfrom`; do
+
+		if [[ -f $i && "$i" =~ $filepattern ]]; then
+			aws s3 cp $uploadfrom/$i $bucketfullpath$i 
+	 	fi
+		
+		if [[ "$i" =~ $directorypattern && -d $i ]]; then
+			aws s3 cp $uploadfrom/$i $bucketfullpath$i --recursive
+	 	fi
+	done
+	
+	#set read permissions for all files
+	for i in `aws s3api list-objects --bucket $bucket --query 'Contents[].{Key: Key}'`; do		
+
+		if [[ "$i" =~ $filepattern && ! "$i" =~ $excludeslash ]]; then
+			file=${i%\"} #strips quote from end
+	 		file=${file#\"} # strips quote from beginning
+	 		echo "Setting $file to public"
+			aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
+	 	fi
+
+	 	if [[ $i =~ $directorypattern\/ ]]; then
+	 		file=${i%\"} #strips quote from end
+	 		file=${file#\"} # strips quote from beginning
+	 		echo "Setting $file to public"
+	 		aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
+	 	fi
+	done
 else
-	echo "file does not exist"
-	fi
-else
-	echo "No arguments"
+	echo "$1 is not a directory"
 fi
