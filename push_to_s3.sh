7b82c0a6c5c12d9dffa35701260369bc81870e1f
Anthony Baker <anthony@Anthony-Bakers-MacBook-Pro.local>
2016-08-21 13:41:17 -0400

    Updating CSS

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-#!/bin/sh
 #change user,group and perms for multplie directoies
 
-directorypattern="(css|javascript)"
-filepattern="(html|json)"
+directorypattern="(css)"
+# filepattern="(html|json)"
 excludeslash="\/"
 bucket="superfastcola.com"
 bucketfullpath="s3://$bucket/"
@@ -18,14 +16,20 @@ if [ -d "$uploadfrom" ]; then
 	 	fi
 		
 		if [[ "$i" =~ $directorypattern && -d $i ]]; then
-			aws s3 cp $uploadfrom/$i $bucketfullpath$i --recursive
+			aws s3 cp $uploadfrom/$i $bucketfullpath$i --recursive \
+			--exclude "*.DS*" \
+			--exclude "data-uri/*" \
+			--exclude "bourbon/*" \
+			--exclude "neat/*" \
+			--exclude "src/*" \
+			--exclude "scss/*"
 	 	fi
 	done
 	
 	#set read permissions for all files
 	for i in `aws s3api list-objects --bucket $bucket --query 'Contents[].{Key: Key}'`; do		
 
-		if [[ "$i" =~ $filepattern && ! "$i" =~ $excludeslash ]]; then
+		if [[ -n $filepattern && "$i" =~ $filepattern && ! "$i" =~ $excludeslash ]]; then
 			file=${i%\"} #strips quote from end
 	 		file=${file#\"} # strips quote from beginning
 	 		echo "Setting $file to public"
