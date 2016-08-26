d00dd2eaa806378f03bda5122f6ab9c2ed385a8d
Anthony Baker <anthony@Anthony-Bakers-MacBook-Pro.local>
2016-08-26 09:55:42 -0400

    Updating s3 command to set permissions on upload

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #change user,group and perms for multplie directoies
 
-directorypattern="(javascript)"
-# filepattern="(html|json|pdf)"
+directorypattern="(javascript|css|images)"
+filepattern="(html|json|pdf)"
 excludeslash="\/"
 bucket="portfolio.deluxeluxury.com"
 bucketfullpath="s3://$bucket/"
@@ -13,39 +13,43 @@ if [ -d "$uploadfrom" ]; then
 
 		if [[ -f $i && "$i" =~ $filepattern ]]; then
 			aws s3 cp $uploadfrom/$i $bucketfullpath$i \
+			--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
 			--exclude "*package.json"
 	 	fi
 		
 		if [[ "$i" =~ $directorypattern && -d $i ]]; then
-			aws s3 cp $uploadfrom/$i $bucketfullpath$i --recursive \
+			aws s3 cp $uploadfrom/$i $bucketfullpath$i \
+			--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
+			--recursive \
 			--exclude "*.DS*" \
 			--exclude "*sublime*" \
 			--exclude "data-uri/*" \
 			--exclude "*package.json" \
 			--exclude "bourbon/*" \
 			--exclude "neat/*" \
+			--exclude "*.psd" \
 			--exclude "src/*" \
 			--exclude "scss/*"
 	 	fi
 	done
 	
-	#set read permissions for all files
-	for i in `aws s3api list-objects --bucket $bucket --query 'Contents[].{Key: Key}'`; do		
+	# #set read permissions for all files
+	# for i in `aws s3api list-objects --bucket $bucket --query 'Contents[].{Key: Key}'`; do		
 
-		if [[ -n $filepattern && "$i" =~ $filepattern && ! "$i" =~ $excludeslash ]]; then
-			file=${i%\"} #strips quote from end
-	 		file=${file#\"} # strips quote from beginning
-	 		echo "Setting $file to public"
-			aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
-	 	fi
+	# 	if [[ -n $filepattern && "$i" =~ $filepattern && ! "$i" =~ $excludeslash ]]; then
+	# 		file=${i%\"} #strips quote from end
+	#  		file=${file#\"} # strips quote from beginning
+	#  		echo "Setting $file to public"
+	# 		aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
+	#  	fi
 
-	 	if [[ $i =~ $directorypattern\/ ]]; then
-	 		file=${i%\"} #strips quote from end
-	 		file=${file#\"} # strips quote from beginning
-	 		echo "Setting $file to public"
-	 		aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
-	 	fi
-	done
+	#  	if [[ $i =~ $directorypattern\/ ]]; then
+	#  		file=${i%\"} #strips quote from end
+	#  		file=${file#\"} # strips quote from beginning
+	#  		echo "Setting $file to public"
+	#  		aws s3api put-object-acl --bucket $bucket --key $file --grant-read 'uri="http://acs.amazonaws.com/groups/global/AllUsers"'
+	#  	fi
+	# done
 else
 	echo "$1 is not a directory"
 fi
